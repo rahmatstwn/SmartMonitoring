1c547e24e4cfc70c683d87d6e30c07ac5ceca9cb
rahmatstwn <rahmat.118140097@student.itera.ac.id>
2022-12-25 05:19:19 +0700

    hapus folder database

--- src/config/database/DataSuhu/index.js
+++
@@ -1,7 +0,0 @@
-import database from '@react-native-firebase/database'
-
-database()
-  .ref('/Hasil_Pembacaan/Temperature')
-  .on('value', snapshot => {
-    console.log('User data: ', snapshot.val());
-  });
