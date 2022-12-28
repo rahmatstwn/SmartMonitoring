de9c191babdb153e39e6c269d71bfe364f50fe90
rahmatstwn <rahmat.118140097@student.itera.ac.id>
2022-12-29 00:49:35 +0700

    fix algoritma AHP (-notif & UI)

--- src/component/forUkuran/index.js
+++ src/component/forUkuran/index.js
@@ -19,153 +19,175 @@ export default class ForUkuran extends Component {
             tinggi4: 0,
             tinggi5: 0,
             Umur: 30, //#hari
-            // data:[2,2],
-            // matriks:[],
-            // kuadratmatriks:[],
-            // eigenVector:[],
-            // eigenVectorSebelumnya:[],
-            // bSt : 0,
-            // totalkuadratMatriks:0.0,
-            // ahp:[],
-            // stat:[]
         }
     }
-    componentDidMount() {
-        this.Tinggi1();
-        this.Tinggi2();
-        this.Tinggi3();
-        this.Tinggi4();
-        this.Tinggi5();
-        this.Diameter1();
-        this.Diameter2();
-        this.Diameter3();
-        this.Diameter4();
-        this.Diameter5();
-        this.fungAHP();
+    async componentDidMount() {
+        try {
+            await this.Tinggi1();
+            await this.Tinggi2();
+            await this.Tinggi3();
+            await this.Tinggi4();
+            await this.Tinggi5();
+            await this.Diameter1();
+            await this.Diameter2();
+            await this.Diameter3();
+            await this.Diameter4();
+            await this.Diameter5();
+        } catch (error) {
+
+        }
+
+
+    }
+    componentDidUpdate() {
+        console.log("masuk update")
+        if (this.state.diameter1 != 0 && this.state.diameter2 != 0 && this.state.diameter3 != 0 && this.state.diameter4 != 0 && this.state.diameter5 != 0) {
+            console.log("masuk ahp")
+            this.fungAHP();
+        }
+
     }
 
-    Tinggi1 =()=>{
+    async Tinggi1() {
+
         database()
-        .ref('/Hasil_Pembacaan/Tanaman1/Tinggi1')
-        .on('value', snapshot => {
-        
-        this.setState({
-            tinggi1: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman1/Tinggi1')
+            .on('value', snapshot => {
+
+                this.setState({
+                    tinggi1: snapshot.val()
+                })
+            });
     }
 
-    Tinggi2 =()=>{
+    async Tinggi2() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman2/Tinggi2')
-        .on('value', snapshot => {
-        
-        this.setState({
-            tinggi2: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman2/Tinggi2')
+            .on('value', snapshot => {
+
+                this.setState({
+                    tinggi2: snapshot.val()
+                })
+            });
     }
 
-    Tinggi3 =()=>{
+    async Tinggi3() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman3/Tinggi3')
-        .on('value', snapshot => {
-        
-        this.setState({
-            tinggi3: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman3/Tinggi3')
+            .on('value', snapshot => {
+
+                this.setState({
+                    tinggi3: snapshot.val()
+                })
+            });
     }
 
-    Tinggi4 =()=>{
+    async Tinggi4() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman4/Tinggi4')
-        .on('value', snapshot => {
-        
-        this.setState({
-            tinggi4: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman4/Tinggi4')
+            .on('value', snapshot => {
+
+                this.setState({
+                    tinggi4: snapshot.val()
+                })
+            });
     }
 
-    Tinggi5 =()=>{
+    async Tinggi5() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman5/Tinggi5')
-        .on('value', snapshot => {
-        
-        this.setState({
-            tinggi5: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman5/Tinggi5')
+            .on('value', snapshot => {
+
+                this.setState({
+                    tinggi5: snapshot.val()
+                })
+            });
     }
 
-    Diameter1 =()=>{
+    async Diameter1() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman1/Diameter1')
-        .on('value', snapshot => {
-        
-        this.setState({
-            diameter1: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman1/Diameter1')
+            .on('value', snapshot => {
+
+                this.setState({
+                    diameter1: snapshot.val()
+                })
+            });
     }
 
-    Diameter2 =()=>{
+    async Diameter2() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman2/Diameter2')
-        .on('value', snapshot => {
-        
-        this.setState({
-            diameter2: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman2/Diameter2')
+            .on('value', snapshot => {
+
+                this.setState({
+                    diameter2: snapshot.val()
+                })
+            });
     }
 
-    Diameter3 =()=>{
+    async Diameter3() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman3/Diameter3')
-        .on('value', snapshot => {
-        
-        this.setState({
-            diameter3: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman3/Diameter3')
+            .on('value', snapshot => {
+
+                this.setState({
+                    diameter3: snapshot.val()
+                })
+            });
     }
 
-    Diameter4 =()=>{
+    async Diameter4() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman4/Diameter4')
-        .on('value', snapshot => {
-        
-        this.setState({
-            diameter4: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman4/Diameter4')
+            .on('value', snapshot => {
+
+                this.setState({
+                    diameter4: snapshot.val()
+                })
+            });
     }
 
-    Diameter5 =()=>{
+    async Diameter5() {
         database()
-        .ref('/Hasil_Pembacaan/Tanaman5/Diameter5')
-        .on('value', snapshot => {
-        
-        this.setState({
-            diameter5: snapshot.val()
-            })
-        });
+            .ref('/Hasil_Pembacaan/Tanaman5/Diameter5')
+            .on('value', snapshot => {
+
+                this.setState({
+                    diameter5: snapshot.val()
+                })
+            });
     }
 
     fungAHP = () => {
-        const jumlData = 3;
+        const jumlData = 5;
         const jumlKriteria = 3;
+        let bSt = false;
+        let matriksData = [];
+        let kuadratmatriks =[];
+        let totalkuadratMatriks = 0;
+        let eigenVector = [];
+        let eigenVectorSebelumnya = [];
         let data = [];
-        let i,j;
+        let i, j;
+        let tanaman = [];
 
-        for(i=0; i<jumlKriteria; i++){
+        for (i = 0; i < jumlData; i++) {
             data[i] = [];
-            for(j=0; j<jumlKriteria; j++){
+            tanaman[i] = "Tanaman ke" + (i+1) ;
+            for (j = 0; j < jumlKriteria; j++) {
                 data[i][j] = null;
             }
         }
+        for (i = 0; i < jumlKriteria; i++) {
+            matriksData[i] = [];
+            kuadratmatriks[i] = []
+            for (j = 0; j < jumlKriteria; j++) {
+                matriksData[i][j] = 0;
+                kuadratmatriks[i][j] = 0;
+            }
+        }
+        console.log(tanaman)
 
         data[0][0] = this.state.tinggi1;
         data[0][1] = this.state.diameter1;
@@ -176,58 +198,54 @@ export default class ForUkuran extends Component {
         data[2][0] = this.state.tinggi3;
         data[2][1] = this.state.diameter3;
         data[2][2] = this.state.Umur;
+        data[3][0] = this.state.tinggi4;
+        data[3][1] = this.state.diameter4;
+        data[3][2] = this.state.Umur;
+        data[4][0] = this.state.tinggi5;
+        data[4][1] = this.state.diameter5;
+        data[4][2] = this.state.Umur;
+        
 
-        for(i=0; i<jumlKriteria-1; i++){
-            for(j=0; j<jumlKriteria-1; j++){
+
+        for (i = 0; i < jumlKriteria - 1; i++) {
+            for (j = 0; j < jumlKriteria - 1; j++) {
                 data[i][j]
             }
         }
 
         console.log(data);
-     
- 
-        let bSt = false;
-        let matriks = [];
-        let kuadratmatriks =[];
-        let totalkuadratMatriks
-        let eigenVector = [];
-        let eigenVectorSebelumnya = [];
+        console.log("matriks perbandingan awal sebelum di input")
+        console.log(matriksData);
+        console.log("matriks kuadrat awal sebelum di input")
+        console.log(kuadratmatriks);
 
-        while(bSt != false) {
-            for(i=0; i<jumlKriteria; i++){
-                matriks[i] = [];
-                kuadratmatriks[i] = [];
-                for(j=0; j<jumlKriteria; j++){
-                    matriks[i][j] = null;
-                    kuadratmatriks[i][j] = null;
-                }
-            }
-            if(matriks[0][0] = 0){
-                matriks[0][0] = 1;
-                matriks[0][1] = 0.33;
-                matriks[0][2] = 0.33;
-                matriks[1][0] = 3;
-                matriks[1][1] = 1;
-                matriks[1][2] = 0.5;
-                matriks[2][0] = 3;
-                matriks[2][1] = 2;
-                matriks[2][2] = 1;
+        do{
+            if(matriksData[0][0] == 0){
+                matriksData[0][0] = 1;
+                matriksData[0][1] = 0.33;
+                matriksData[0][2] = 0.33;
+                matriksData[1][0] = 3;
+                matriksData[1][1] = 1;
+                matriksData[1][2] = 0.5;
+                matriksData[2][0] = 3;
+                matriksData[2][1] = 2;
+                matriksData[2][2] = 1;
 
             }else{
-                matriks[0][0] = kuadratmatriks[0][0];
-                matriks[0][1] = kuadratmatriks[0][1];
-                matriks[0][2] = kuadratmatriks[0][2];
-                matriks[1][0] = kuadratmatriks[1][0];
-                matriks[1][1] = kuadratmatriks[1][1];
-                matriks[1][2] = kuadratmatriks[1][2];
-                matriks[2][0] = kuadratmatriks[2][0];
-                matriks[2][1] = kuadratmatriks[2][1];
-                matriks[2][2] = kuadratmatriks[2][2];
+                matriksData[0][0] = kuadratmatriks[0][0];
+                matriksData[0][1] = kuadratmatriks[0][1];
+                matriksData[0][2] = kuadratmatriks[0][2];
+                matriksData[1][0] = kuadratmatriks[1][0];
+                matriksData[1][1] = kuadratmatriks[1][1];
+                matriksData[1][2] = kuadratmatriks[1][2];
+                matriksData[2][0] = kuadratmatriks[2][0];
+                matriksData[2][1] = kuadratmatriks[2][1];
+                matriksData[2][2] = kuadratmatriks[2][2];
             }
-            
+
             for(i=0; i<=2; i++){
                 for(j=0; j<=2; j++){
-                    kuadratmatriks[i][j] = (matriks[0, j] * matriks[i, 0]) + (matriks[1, j] * matriks[i, 1]) + (matriks[2, j] * matriks[i, 2]);
+                    kuadratmatriks[i][j] = (matriksData[0][j] * matriksData[i][0]) + (matriksData[1][j] * matriksData[i][1]) + (matriksData[2][j] * matriksData[i][2]);
                     totalkuadratMatriks += kuadratmatriks[i][j];
                 }
             }
@@ -242,17 +260,23 @@ export default class ForUkuran extends Component {
                 eigenVectorSebelumnya[1] = eigenVector[1]
                 eigenVectorSebelumnya[2] = eigenVector[2]
             }
-        }
-        
+        }while(bSt != false);
+        console.log("matriks perbandingan awal sesudah di input")
+        console.log(matriksData);
+        console.log("matriks kuadrat awal sesudah di input")
+        console.log(kuadratmatriks);
+        console.log("Nilai Eigen");
+        console.log(eigenVector)
+
         const maksUmur = 50;
-        const maksDiameter = 30;
+        const maksDiameter = 28;
         const maksTinggi = 20;
 
         let ahp = [];
         let stat = [];
 
         for(i=0; i<=jumlData-1; i++){
-            ahp[i] = (data[i, 0] / maksTinggi * eigenVector[0]) + (data[i, 1] / maksDiameter) * eigenVector[1] + (data[i, 2] / maksUmur) * eigenVector[2]
+            ahp[i] = (data[i][0] / maksTinggi * eigenVector[0]) + (data[i][1] / maksDiameter) * eigenVector[1] + (data[i][2] / maksUmur) * eigenVector[2]
             if(ahp[i] < 0.6){
                 stat[i] = "Kurang";
             }else if(ahp[i] < 0.7){
@@ -267,96 +291,75 @@ export default class ForUkuran extends Component {
         console.log("Tanaman 1" && ahp[0])
         console.log("Tanaman 2" && ahp[1])
         console.log("Tanaman 3" && ahp[2])
+        console.log("Tanaman 2" && ahp[3])
+        console.log("Tanaman 3" && ahp[4])
 
         console.log("Kesimpulan Akhir")
         console.log("Tanaman 1" && stat[0])
         console.log("Tanaman 2" && stat[1])
         console.log("Tanaman 3" && stat[2])
+        console.log("Tanaman 2" && stat[3])
+        console.log("Tanaman 3" && stat[4])
 
-        
+        let temp;
+        for(i=0; i<jumlData; i++){
+            for(j=0; j<jumlData; j++){
+                if(ahp[i] > ahp[j]){
+                    temp = ahp[i];
+                    ahp[i] = ahp[j];
+                    ahp[j] = temp;
+                }
+            }
+        }
 
-        
-        // // const ahpContext = new AHP();
 
-        // // ahpContext.addItems(['Tanaman1', 'Tanaman2', 'Tanaman3']);
-
-        // // ahpContext.addCriteria(['Tinggi', 'Diameter', 'Umur']);
-
-        // // //rank criteria with rank scale
-        // // ahpContext.rankCriteriaItem('Tinggi', [
-        // //     ['Tanaman2', 'Tanaman3', 1.665],//this.state.tinggi1 * 0.111],
-        // //     ['Tanaman1', 'Tanaman3', 1.887],//this.state.tinggi2 * 0.111],
-        // //     ['Tanaman1', 'Tanaman2', 1.887]//this.state.tinggi3 * 0.111]
-        // // ]);
-
-        // // //rank criteria with rank scale
-        // // ahpContext.rankCriteriaItem('Diameter', [
-        // //     ['Tanaman2', 'Tanaman3', 5.994],//this.state.diameter1 * 0.333],
-        // //     ['Tanaman1', 'Tanaman3', 6.66],//this.state.diameter2 * 0.333],
-        // //     ['Tanaman1', 'Tanaman2', 8.325]//this.state.diameter3 * 0.333]
-        // // ]);
-
-        // // //rank criteria with absolute rank scole
-        // // ahpContext.setCriteriaItemRankByGivenScores('Umur', [16.68, 16.68, 16.68]);
-
-        // // ahpContext.rankCriteria(
-        // //     [
-        // //         ['Tinggi', 'Diameter', 5],
-        // //         ['Tinggi', 'Umur', 3],
-        // //         ['Diameter', 'Umur', 1]
-        // //     ]
-        // // );
-
-        // // const output = ahpContext.run();
-        // // console.log(output);
-        
     }
-  render() {
-    return (
-        <SafeAreaView>
-            <View style = {styles.kotak}>
-                <View style={{flexDirection:'row'}}>
-                    <View style={{flexDirection:'column', marginHorizontal:moderateScale(30)}}>
-                        <Text style={{fontWeight:'bold'}}>Data Tanaman 1</Text>
-                        <Text>Diameter : {this.state.diameter1}</Text>
-                        <Text>Tinggi : {this.state.tinggi1}</Text>
+    render() {
+        return (
+            <SafeAreaView>
+                <View style={styles.kotak}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style={{ flexDirection: 'column', marginHorizontal: moderateScale(30) }}>
+                            <Text style={{ fontWeight: 'bold' }}>Data Tanaman 1</Text>
+                            <Text>Diameter : {this.state.diameter1}</Text>
+                            <Text>Tinggi : {this.state.tinggi1}</Text>
+                        </View>
+                        <View style={{ flexDirection: 'column', marginHorizontal: moderateScale(30) }}>
+                            <Text style={{ fontWeight: 'bold' }}>Data Tanaman 2</Text>
+                            <Text>Diameter : {this.state.diameter2}</Text>
+                            <Text>Tinggi : {this.state.tinggi2}</Text>
+                        </View>
                     </View>
-                    <View style={{flexDirection:'column', marginHorizontal:moderateScale(30)}}>
-                    <Text style={{fontWeight:'bold'}}>Data Tanaman 2</Text>
-                        <Text>Diameter : {this.state.diameter2}</Text>
-                        <Text>Tinggi : {this.state.tinggi2}</Text>
+                    <View style={{ flexDirection: 'row', marginVertical: moderateScale(30) }}>
+                        <View style={{ flexDirection: 'column', marginHorizontal: moderateScale(30) }}>
+                            <Text style={{ fontWeight: 'bold' }}>Data Tanaman 3</Text>
+                            <Text>Diameter : {this.state.diameter3}</Text>
+                            <Text>Tinggi : {this.state.tinggi3}</Text>
+                        </View>
+                        <View style={{ flexDirection: 'column', marginHorizontal: moderateScale(30) }}>
+                            <Text style={{ fontWeight: 'bold' }}>Data Tanaman 4</Text>
+                            <Text>Diameter : {this.state.diameter4}</Text>
+                            <Text>Tinggi : {this.state.tinggi4}</Text>
+                        </View>
                     </View>
+                    <View>
+                        <Text style={{ fontWeight: 'bold', alignItems: 'flex-start' }}>Data Tanaman 5</Text>
+                        <Text>Diameter : {this.state.diameter5}</Text>
+                        <Text>Tinggi : {this.state.tinggi5}</Text>
+                    </View>
+
                 </View>
-                <View style={{flexDirection:'row', marginVertical:moderateScale(30)}}>
-                    <View style={{flexDirection:'column', marginHorizontal:moderateScale(30)}}>
-                        <Text style={{fontWeight:'bold'}}>Data Tanaman 3</Text>
-                        <Text>Diameter : {this.state.diameter3}</Text>
-                        <Text>Tinggi : {this.state.tinggi3}</Text>
-                    </View>
-                    <View style={{flexDirection:'column', marginHorizontal:moderateScale(30)}}>
-                        <Text style={{fontWeight:'bold'}}>Data Tanaman 4</Text>
-                        <Text>Diameter : {this.state.diameter4}</Text>
-                        <Text>Tinggi : {this.state.tinggi4}</Text>
-                    </View>
+                <View style={{ alignItems: 'center', marginTop: moderateScale(100) }}>
+                    <Text style={{ fontWeight: 'bold', fontSize: 15 }}>Rahmat Setiawan - 118140097</Text>
                 </View>
-                <View>
-                    <Text style={{fontWeight:'bold', alignItems:'flex-start'}}>Data Tanaman 5</Text>
-                    <Text>Diameter : {this.state.diameter5}</Text>
-                    <Text>Tinggi : {this.state.tinggi5}</Text>
-                </View>
-                
-            </View>
-            <View style={{alignItems:'center', marginTop:moderateScale(100)}}>
-                <Text style={{fontWeight:'bold', fontSize:15}}>Rahmat Setiawan - 118140097</Text>
-            </View>
-        </SafeAreaView>
-      )
-  }
+            </SafeAreaView>
+        )
+    }
 }
 const styles = StyleSheet.create({
-    kotak:{
-        marginTop:moderateScale(30),
-        backgroundColor : '#DEF1DF',
+    kotak: {
+        marginTop: moderateScale(30),
+        backgroundColor: '#DEF1DF',
         marginHorizontal: moderateScale(16),
         borderRadius: 8,
         width: 350,
