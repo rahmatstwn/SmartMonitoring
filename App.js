22d5be8328c17916e0e163a6cf0ebc3b3c00b270
rahmatstwn <rahmat.118140097@student.itera.ac.id>
2023-02-06 15:48:02 +0700

    update try notification

--- App.js
+++ App.js
@@ -2,10 +2,36 @@ import React, {useEffect} from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import SplashScreen from 'react-native-splash-screen';
 import Router from './src/config/router'
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
 
 const App = () => {
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+  async function getToken(){
+    const fcmToken = await messaging().getToken();
+    console.log(fcmToken);
+  }
+
   useEffect (() => {
+    requestUserPermission();
+    getToken();
+    
     SplashScreen.hide();
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', remoteMessage.notification?.body);
+    });
+
+    return unsubscribe;
   }, [])
   
   return (
